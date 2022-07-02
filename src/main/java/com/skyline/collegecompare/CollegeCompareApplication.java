37b04bf7e18e801fb045babb662d95c77d51acc1
skyline-blue <2895054643@qq.com>
2022-07-02 11:44:54 +0800

    add branch master and add some basic things

---
+++ src/main/java/com/skyline/collegecompare/CollegeCompareApplication.java
@@ -0,0 +1,13 @@
+package com.skyline.collegecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeCompareApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/skyline/collegecompare/data/dao/CollegeInfo.java
@@ -0,0 +1,21 @@
+package com.skyline.collegecompare.data.dao;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * [FEATURE INFO]<br/>
+ * College Info
+ *
+ * @author Skyline
+ * @create 2022/7/2 10:59
+ * @since 1.0.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("college_info_table")
+public class CollegeInfo {
+}
